3e6332cfa9f4e31c4d9204c9e75517a44a48f99f
olonyl <olony.rocha@contollo.net>
2022-02-17 21:39:02 -0600

    Using Null Conditional Operator in Arrays

--- WorkingWithNullable.GameConsole/Program.cs
+++ WorkingWithNullable.GameConsole/Program.cs
@@ -6,14 +6,29 @@ namespace WorkingWithNullable.GameConsole
     {
         static void Main(string[] args)
         {
-            PlayerCharacter player = new PlayerCharacter();
+            PlayerCharacter[] players = new PlayerCharacter[] {
+            new PlayerCharacter{Name="Olonyl"},
+            new PlayerCharacter(),
+            null
+            };
 
-            player.DaysSinceLastLogin = 42;
+
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
+
+
+            PlayerCharacter[] players2 = null;
+
+
+            string p21 = players2?[0]?.Name;
+            string p22 = players2?[1]?.Name;
+            string p23 = players2?[2]?.Name;
 
 
             //Null Conditional Operator + Null Coalescing Operator
+            PlayerCharacter player = null;
             int days = player?.DaysSinceLastLogin ?? -1;
-
             Console.WriteLine(days);
 
             Console.ReadLine();
